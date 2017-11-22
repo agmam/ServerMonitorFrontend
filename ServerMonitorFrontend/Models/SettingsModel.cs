738c84d09091d19e73fd02b5dc660596219d386f
EsbenLaursen <Esben.laursen@gmail.com>
2017-11-22 09:48:42 +0100

    6+59

--- ServerMonitorFrontend/Models/SettingsModel.cs
+++ ServerMonitorFrontend/Models/SettingsModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Entities.Entities;
 
 namespace ServerMonitorFrontend.Models
 {
     public class SettingsModel
     {
+        public List<EventType> EventTypes { get; set; }
     }
 }
