8f0b7c5744eb2607998bf9c4f343d8043f6c449c
agmam <ag-mam@live.dk>
2017-12-15 11:21:02 +0100

    delete server

---
+++ ServerMonitorFrontend/Controllers/ServerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServerMonitorFrontend.Gateways;
+
+namespace ServerMonitorFrontend.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly IServerGateway serverGateway =
+           new BLLFacade().GetServerGateway();
+        // GET: Server
+        public ActionResult Index()
+        {
+            var model = serverGateway.ReadAll();
+            return View(model);
+        }
+
+        public ActionResult DeleteServer(int id)
+        {
+            var s = serverGateway.Read(id);
+            if (s != null) { 
+                serverGateway.Delete(s);
+            }
+           return RedirectToAction("Index");
+        }
+    }
+}
